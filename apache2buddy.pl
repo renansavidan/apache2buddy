541fc3170efb3618a31d74ef40c3516faf029cd0
RENAN SAVIDAN <renansavidan@users.noreply.github.com>
2020-08-12 22:32:18 -0300

    Update apache2buddy.pl

--- apache2buddy.pl
+++ apache2buddy.pl
@@ -1801,7 +1801,7 @@ sub preflight_checks {
 			if ( ! $NOINFO ) { show_info_box; print "The process running on port ${CYAN}$port${ENDC} is ${CYAN}$apache_version${ENDC}.\n" }
 		}  else {
 			if ( ! $NOINFO ) { show_info_box; print "The process running on port $port is not Apache. Falling back to process list...\n" }
-			my @process_info = split(' ', `ps -C 'httpd httpd.worker apache apache2 /usr/sbin/httpd /usr/sbin/httpd.worker' -f | grep '^root'`);
+			my @process_info = split(' ', `ps -C 'httpd.bin httpd.worker apache apache2 /usr/sbin/httpd /usr/sbin/httpd.worker' -f | grep '^root'`);
 			$pid = $process_info[1];
 	
 			if ( !$pid ) {
